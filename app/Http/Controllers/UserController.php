5ee06e118b319a9c2a523a248259d1cfc63a8533
victor braz <victor.90braz@gmail.com>
2023-09-06 15:09:34 +0200

    validate form

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,13 +6,20 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-  public function home()
-    {
+  public function home() {
         return view('home/home');
     }
 
-    public function register()
-    {
-        return "Hello from our controller";
+    public function register(Request $request) {
+
+      $checkFields = [
+        "name"=> ["required", "min:3", "max:20"],
+        "email"=> ["required", "email"],
+        "password"=> ["required", "min:3", "max:20"],
+      ];
+
+      $incomingFields = $request->validate($checkFields);
+
+      return  $incomingFields;
     }
 }
