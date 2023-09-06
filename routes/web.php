0eda3b672c1a2b4a98375a30f3d90c54027734df
victor braz <victor.90braz@gmail.com>
2023-09-06 15:00:17 +0200

    create a usercontroller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 		return view('home/home');
 });
 
-Route::post('/register', function () {
-  return "thanks to register";
-});
+Route::post('/register', [UserController::class, "register"]);
