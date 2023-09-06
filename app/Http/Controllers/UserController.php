34e0322034dc32a8519237f1938ec69b970c2b13
victor braz <victor.90braz@gmail.com>
2023-09-06 15:24:46 +0200

    add auth for user login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -14,16 +15,17 @@ class UserController extends Controller
     public function register(Request $request) {
 
       $checkFields = [
-        "name"=> ["required", "min:3", "max:20"],
-        "email"=> ["required", "email"],
+        "name"=> ["required", "min:3", "max:20", Rule::unique("users", "name")],
+        "email"=> ["required", "email", Rule::unique("users", "email")],
         "password"=> ["required", "min:3", "max:20"],
       ];
 
       $incomingFields = $request->validate($checkFields);
       $incomingFields["password"] = bcrypt($incomingFields["password"]);
 
-      User::create($incomingFields);
+      $user = User::create($incomingFields);
+      auth()->login($user);
 
-      return  $incomingFields;
+      return redirect("/");
     }
 }
